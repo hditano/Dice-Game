5957582907d1b595742e126c5deb7dd7159d6195
Hernan Di Tano <hditano@gmail.com>
2021-08-19 04:10:26 -0300

    added resetGame() and ShowResetButton() functions

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const diceButton = document.querySelector('.dice-btn');
+const resetButton = document.querySelector('.reset-btn');
+const h1 = document.querySelector('h1');
 let totalP1 = document.querySelector('.total-p1');
 let totalP2 = document.querySelector('.total-p2');
 
@@ -16,22 +18,56 @@ function randomDice() {
     return Math.floor(Math.random() * 5) + 1;
 }
 
+function showResetButton() {
+    diceButton.style.display = 'none';
+    resetButton.style.display = 'block';
+}
+
+resetButton.addEventListener('click', function() {
+    resetGame();
+    resetButton.style.display = 'none';
+    diceButton.style.display = 'block';
+})
+
 diceButton.addEventListener('click', function () {
     if (player1Turn === true) {
+        h1.textContent = 'Player 1 Turn!!!';
         totalP2.classList.remove('total-shadow');
         totalP1.classList.add('total-shadow');
         let dice = randomDice();
-        let scoreP1 = player1 += dice;
+        player1 += dice;
         player1Score.textContent = `${player1}`;
         totalP1.textContent = dice;
         player1Turn = false;
     } else {
+        h1.textContent = 'Player 2 Turn!!!';
         totalP1.classList.remove('total-shadow');
         totalP2.classList.add('total-shadow');
         let dice = randomDice();
-        let scoreP1 = player2 += dice;
+        player2 += dice;
         player2Score.textContent = `${player2}`;
         totalP2.textContent = dice;
         player1Turn = true;
     }
+
+    if(player1 >= 20) {
+        h1.textContent = 'Player 1 Won!!!';
+        showResetButton();
+    } else if(player2 >= 20){
+        h1.textContent = 'Player 2 Won!!!';
+        showResetButton();
+    }
 })
+
+
+function resetGame() {
+    player1 = 0;
+    player2 = 0;
+    totalP1.textContent = '-';
+    totalP2.textContent = '-';
+    player1Score.textContent = 0;
+    player2Score.textContent = 0;
+    player1Turn = true;
+    h1.textContent = 'Player 1 Turn';
+    diceButton.textContent = 'Roll Dice';
+}
